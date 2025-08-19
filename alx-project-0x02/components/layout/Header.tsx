b8528ddae89b7f48575331ca8839540f3811ad0a
George-Okumu <gokumu12@gmail.com>
2025-08-19 10:39:42 +0300

    Basic Routing in Next.js

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,7 +1,10 @@
 export default function Header(){
     return (
         <>
-        
+        <ul>
+            <li><a href="/about">About</a></li>
+            <li><a href="/home">Home</a></li>
+        </ul>
         </>
     )
 }
